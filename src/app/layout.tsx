622c178d00f185eec976c20b5ac8d7fec3610dd5
SamiaIslam22 <samiaislam4477@gmail.com>
2025-08-27 01:32:02 -0400

    Fix character encoding for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased responsive-zoom`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="responsive-zoom">
+          {children}
+        </div>
       </body>
     </html>
   );
